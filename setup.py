ef0518eb6331516dd27b100dc03f7b188d77cdc3
Enric Tobella <etobella@creublanca.es>
2021-03-04 09:33:36 +0100

    BUMP version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ tests_require = [
 
 setup(
     name="xmlsig",
-    version="0.1.2",
+    version="0.1.4",
     description="Python based XML signature",
     long_description="XML Signature created with cryptography and lxml",
     author="Enric Tobella Alomar",
